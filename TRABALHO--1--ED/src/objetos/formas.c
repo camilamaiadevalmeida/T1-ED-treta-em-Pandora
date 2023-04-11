bc9d94cbf03838c572c50e2d91cb59fd68000eb1
tonhão mototaxi <jean.carlo.polo@uel.br>
2023-04-11 03:45:44 -0300

    formas arrumado eu acho

--- TRABALHO--1--ED/src/objetos/formas.c
+++ TRABALHO--1--ED/src/objetos/formas.c
@@ -1,418 +1,340 @@
 #include "formas.h"
 #include "../arqs/main.h"
 
-
 typedef struct retangulo
 {
-    char x[20];
-    char tipo[20];
-    char y[20];
-    char corb[20];
-    char corp[20];
-    char id[20];
     char w[20];
     char h[20];
-    char rota[20];
-}Rect;
+} Rect;
 
-typedef struct circulo {
-
-    char id[20];
-    char tipo[20];
+typedef struct circulo
+{
     char r[20];
-    char x[20];
-    char y[20];
-    char corb[20];
-    char corp[20];
-    char rota[20];
-}Circ;
+} Circ;
 
-typedef struct linha{
-
-    char id[20];
-    char tipo[20];
-    char x[20];
-    char y[20];
+typedef struct linha
+{
     char x2[20];
     char y2[20];
-    char cor[20];
-    char rota[20];
-}Linha;
+} Linha;
 
 typedef struct texto
 {
+    char txto[400];
+    char ancora[20];
+    char familia[20];
+    char tamanho[20];
+    char peso[20];
+    char rota[20];
+} Text;
+
+typedef struct formato
+{
+    // coisas gerais
     char id[20];
     char tipo[20];
     char x[20];
     char y[20];
     char corb[20];
     char corp[20];
-    char txto[400];
     char rota[20];
-}Text;
 
-typedef struct formato{
-    //char tipo[20];
-    struct formato *prox;
-    struct formato *ant;
+    // coisas especificas
+    Rect retangulo;
+    Circ circulo;
+    Linha linha;
+    Text texto;
+} Forma;
 
-        Rect retangulo;
-        Circ circulo;
-        Linha linha;
-        Text texto;
-    
-}forma;
+void *cria_retangulo(char i[], char tipo[], char w[], char h[], char x[], char y[], char corb[], char corp[])
+{
+    Forma *retangulo = (Forma *)malloc(sizeof(Forma));
 
+    set_id(retangulo, i);
+    set_tipo(retangulo, tipo);
+    set_x(retangulo, x);
+    set_y(retangulo, y);
+    set_corb(retangulo, corb);
+    set_corp(retangulo, corp);
+    set_w(retangulo, w);
+    set_h(retangulo, h);
 
-void* cria_retangulo(char id[], char tipo[], char w[], char h[],char x[],char y[],char corb[],char corp [],char rota[]){
-    Rect *novo = malloc (sizeof(Rect));
+    set_rota(retangulo, "0");
 
-    void set_id(void* formato, char id[]){
-        struct formato *forma = formato;
-        strcpy(forma->retangulo.id, id);
-    }
-
-    char* get_id(void* formato){
-        struct formato *forma = formato;
-        return forma->retangulo.id;
-    }
-
-    void set_tipo(void* formato, char tipo[]){
-        struct formato *forma = formato;
-        strcpy(forma->retangulo.tipo, tipo);
-    }
-
-    char* get_tipo(void* formato)
-    {
-        struct formato *forma = formato;
-        return forma->retangulo.tipo;
-    }
-    void set_w(void* formato, char w[])
-    {
-        struct formato *forma = formato;
-        strcpy(forma->retangulo.w, w);
-    }
-    char* get_w(void* formato)
-    {
-        struct formato *forma = formato;
-        return forma->retangulo.w;
-    }
-    void set_h(void* formato, char h[])
-    {
-        struct formato *forma = formato;
-        strcpy(forma->retangulo.h, h);
-    }
-    char* get_h(void* formato)
-    {
-        struct formato *forma = formato;
-        return forma->retangulo.h;
-    }
-    void set_x(void* formato, char x[])
-    {
-        struct formato *forma = formato;
-        strcpy(forma->retangulo.x, x);
-    }
-    char* get_x(void* formato)
-    {
-        struct formato *forma = formato;
-        return forma->retangulo.x;
-    }
-    void set_y(void* formato, char y[])
-    {
-        struct formato *forma = formato;
-        strcpy(forma->retangulo.y, y);
-    }
-    char* get_y(void* formato)
-    {
-        struct formato *forma = formato;
-        return forma->retangulo.y;
-    }
-    void set_corb(void* formato, char corb[])
-    {
-        struct formato *forma = formato;
-        strcpy(forma->retangulo.corb, corb);
-    }
-    char* get_corb(void* formato)
-    {
-        struct formato *forma = formato;
-        return forma->retangulo.corb;
-    }
-    void set_corp(void* formato, char corp[])
-    {
-        struct formato *forma = formato;
-        strcpy(forma->retangulo.corp, corp);
-    }
-    char* get_corp(void* formato)
-    {
-        struct formato *forma = formato;
-        return forma->retangulo.corp;
-    }
-    //rotação vem depois
-    if(novo != NULL){
-    return novo;
-    }
-    else
-    {
-        return 0;
-    }
+    return retangulo;
 }
 
-void* cria_circulo(char id[], char tipo[], char r[],char x[],char y[],char corb[],char corp [],char rota[]){
-    Circ *novo = malloc (sizeof(Circ)); 
-    void set_id(void* formato, char id[])
-    {
-        struct formato *forma = formato;
-        strcpy(forma->circulo.id, id);
-    }
+void *cria_circulo(char i[], char tipo[], char r[], char x[], char y[], char corb[], char corp[])
+{
+    Forma *circulo = (Forma *)malloc(sizeof(Forma));
 
-    char* get_id(void* formato)
-    {
-        struct formato *forma = formato;
-        return forma->circulo.id;
-    }
-    void set_tipo(void* formato, char tipo[])
-    {
-        struct formato *forma = formato;
-        strcpy(forma->circulo.tipo, tipo);
-    }
-    char* get_tipo(void* formato)
-    {
-        struct formato *forma = formato;
-        return forma->circulo.tipo;
-    }
-    void set_r(void* formato, char r[])
-    {
-        struct formato *forma = formato;
-        strcpy(forma->circulo.r, r);
-    }
-    char* get_r(void* formato)
-    {
-        struct formato *forma = formato;
-        return forma->circulo.r;
-    }
-    void set_x(void* formato, char x[])
-    {
-        struct formato *forma = formato;
-        strcpy(forma->circulo.x, x);
-    }
-    char* get_x(void* formato)
-    {
-        struct formato *forma = formato;
-        return forma->circulo.x;
-    }
-    void set_y(void* formato, char y[])
-    {
-        struct formato *forma = formato;
-        strcpy(forma->circulo.y, y);
-    }
-    char* get_y(void* formato)
-    {
-        struct formato *forma = formato;
-        return forma->circulo.y;
-    }
-    void set_corb(void* formato, char corb[])
-    {
-        struct formato *forma = formato;
-        strcpy(forma->circulo.corb, corb);
-    }
-    char* get_corb(void* formato)
-    {
-        struct formato *forma = formato;
-        return forma->circulo.corb;
-    }
-    void set_corp(void* formato, char corp[])
-    {
-        struct formato *forma = formato;
-        strcpy(forma->circulo.corp, corp);
-    }
-    char* get_corp(void* formato)
-    {
-        struct formato *forma = formato;
-        return forma->circulo.corp;
-    }
-    //rotação vem depois    == 0 //por enquanto
-    
-    if(novo != NULL){
-    return novo;
-    }
-    else
-    {
-        return 0;
-    }
+    set_id(circulo, i);
+    set_tipo(circulo, tipo);
+    set_x(circulo, x);
+    set_y(circulo, y);
+    set_corb(circulo, corb);
+    set_corp(circulo, corp);
+    set_r(circulo, r);
+
+    set_rota(circulo, "0");
+
+    return circulo;
 }
 
-void* cria_linha (char id[], char tipo[], char x[],char y[],char x2[],char y2[],char cor[],char rota[]){
+void *cria_linha(char i[], char tipo[], char x[], char y[], char x2[], char y2[], char cor[])
+{
+    Forma *linha = (Forma *)malloc(sizeof(Forma));
 
-    Linha* novo = malloc (sizeof(Linha));
-    
-    void set_id(void* formato, char id[])
-    {
-        struct formato *forma = formato;
-        strcpy(forma->linha.id, id);
-    }
-    char* get_id(void* formato)
-    {
-        struct formato *forma = formato;
-        strcpy(forma->linha.id, id);
-    }
-    void set_tipo(void* formato, char tipo[])
-    {
-        struct formato *forma = formato;
-        strcpy(forma->linha.tipo, tipo);
-    }
-    char* get_tipo(void* formato)
-    {
-        struct formato *forma = formato;
-        return forma->linha.tipo;
-    }
-    void set_x(void* formato, char x[])
-    {
-        struct formato *forma = formato;
-        strcpy(forma->linha.x, x);
-    }
-    char* get_x(void* formato)
-    {
-        struct formato *forma = formato;
-        return forma->linha.x;
-    }
-    void set_y(void* formato, char y[])
-    {
-        struct formato *forma = formato;
-        strcpy(forma->linha.y, y);
-    }
-    char* get_y(void* formato)
-    {
-        struct formato *forma = formato;
-        return forma->linha.y;
-    }
-    void set_x2(void* formato, char x2[])
-    {
-        struct formato *forma = formato;
-        strcpy(forma->linha.x2, x2);
-    }
-    char* get_x2(void* formato)
-    {
-        struct formato *forma = formato;
-        return forma->linha.x2;
-    }
-    void set_y2(void* formato, char y2[])
-    {
-        struct formato *forma = formato;
-        strcpy(forma->linha.y2, y2);
-    }
-    char* get_y2(void* formato)
-    {
-        struct formato *forma = formato;
-        return forma->linha.y2;
-    }
-    void set_cor(void* formato, char cor[])
-    {
-        struct formato *forma = formato;
-        strcpy(forma->linha.cor, cor);
-    }
-    char* get_cor(void* formato)
-    {
-        struct formato *forma = formato;
-        return forma->linha.cor;
-    }
-    //rotação vem depois          
+    set_id(linha, i);
+    set_tipo(linha, tipo);
+    set_x(linha, x);
+    set_y(linha, y);
+    set_corb(linha, cor);
+    set_corp(linha, cor);
+    set_x2(linha, x2);
+    set_y2(linha, y2);
 
-    if(novo != NULL){
-    return novo;
-    }
-    else
-    {
-        return 0;
-    }
+    set_rota(linha, "0");
+
+    return linha;
 }
 
-void* cria_texto (char i[], char tipo[], char x[],char y[],char corb[], char corp[],char txto[],char rota[]){
+void *cria_texto(char i[], char tipo[], char x[], char y[], char corb[], char corp[], char txto[], char familia[], char tamanho[], char peso[], char ancora[])
+{
+    Forma *texto = (Forma *)malloc(sizeof(Forma));
 
-    Text* novo = malloc (sizeof(Text));
-    
-    void set_id(void* formato, char id[])
-    {
-        struct formato *forma = formato;
-        strcpy(forma->texto.id, id);
-    }
-    char* get_id(void* formato)
-    {
-        struct formato *forma = formato;
-        return forma->texto.id;
-    }
-    void set_tipo(void* formato, char tipo[])
-    {
-        struct formato *forma = formato;
-        strcpy(forma->texto.tipo, tipo);
-    }
-    char* get_tipo(void* formato)
-    {
-        struct formato *forma = formato;
-        return forma->texto.tipo;
-    }
-    void set_x(void* formato, char x[])
-    {
-        struct formato *forma = formato;
-        strcpy(forma->texto.x, x);
-    }
-    char* get_x(void* formato)
-    {
-        struct formato *forma = formato;
-        return forma->texto.x;
-    }
-    void set_y(void* formato, char y[])
-    {
-        struct formato *forma = formato;
-        strcpy(forma->texto.y, y);
-    }
-    char* get_y(void* formato)
-    {
-        struct formato *forma = formato;
-        return forma->texto.y;
-    }
-    void set_corb(void* formato, char corb[])
-    {
-        struct formato *forma = formato;
-        strcpy(forma->texto.corb, corb);
-    }
-    char* get_corb(void* formato)
-    {
-        struct formato *forma = formato;
-        return forma->texto.corb;
-    }
-    void set_corp(void* formato, char corp[])
-    {
-        struct formato *forma = formato;
-        strcpy(forma->texto.corp, corp);
-    }
-    char* get_corp(void* formato)
-    {
-        struct formato *forma = formato;
-        return forma->texto.corp;
-    }
-    void set_txto(void* formato, char txto[])
-    {
-        struct formato *forma = formato;
-        strcpy(forma->texto.txto, txto);
-    }
-    char* get_txto(void* formato)
-    {
-        struct formato *forma = formato;
-        return forma->texto.txto;
-    }
-    //rotação vem depois 
-    if(novo != NULL){
-    return novo;
-    }
-    else
-    {
-        return 0;
-    }
+    set_id(texto, i);
+    set_tipo(texto, tipo);
+    set_x(texto, x);
+    set_y(texto, y);
+    set_corb(texto, corb);
+    set_corp(texto, corp);
+    set_txto(texto, txto);
+    set_familia(texto, familia);
+    set_tamanho(texto, tamanho);
+    set_peso(texto, peso);
+    set_ancora(texto, ancora);
+
+    set_rota(texto, "0");
+
+    return texto;
 }
 
-//faz sentido eu precisar receber o tipo antes
+// Funções de set:
+
+void set_id(void *formato, char id[])
+{
+    Forma *f = (Forma *)formato;
+    strcpy(f->id, id);
+}
+
+void set_tipo(void *formato, char tipo[])
+{
+    Forma *f = (Forma *)formato;
+    strcpy(f->tipo, tipo);
+}
+
+void set_x(void *formato, char x[])
+{
+    Forma *f = (Forma *)formato;
+    strcpy(f->x, x);
+}
+
+void set_y(void *formato, char y[])
+{
+    Forma *f = (Forma *)formato;
+    strcpy(f->y, y);
+}
+
+void set_corb(void *formato, char corb[])
+{
+    Forma *f = (Forma *)formato;
+    strcpy(f->corb, corb);
+}
+
+void set_corp(void *formato, char corp[])
+{
+    Forma *f = (Forma *)formato;
+    strcpy(f->corp, corp);
+}
+
+void set_rota(void *formato, char rota[])
+{
+    Forma *f = (Forma *)formato;
+    strcpy(f->rota, rota);
+}
+
+// Funções de get:
+
+char *get_id(void *formato)
+{
+    Forma *f = (Forma *)formato;
+    return f->id;
+}
+
+char *get_tipo(void *formato)
+{
+    Forma *f = (Forma *)formato;
+    return f->tipo;
+}
+
+char *get_x(void *formato)
+{
+    Forma *f = (Forma *)formato;
+    return f->x;
+}
+
+char *get_y(void *formato)
+{
+    Forma *f = (Forma *)formato;
+    return f->y;
+}
+
+char *get_corb(void *formato)
+{
+    Forma *f = (Forma *)formato;
+    return f->corb;
+}
+
+char *get_corp(void *formato)
+{
+    Forma *f = (Forma *)formato;
+    return f->corp;
+}
+
+char *get_rota(void *formato)
+{
+    Forma *f = (Forma *)formato;
+    return f->rota;
+}
+
+// Funções de get específicas:
+
+char *get_w(void *formato)
+{
+    Forma *f = (Forma *)formato;
+    return f->retangulo.w;
+}
+
+char *get_h(void *formato)
+{
+    Forma *f = (Forma *)formato;
+    return f->retangulo.h;
+}
+
+char *get_r(void *formato)
+{
+    Forma *f = (Forma *)formato;
+    return f->circulo.r;
+}
+
+char *get_x2(void *formato)
+{
+    Forma *f = (Forma *)formato;
+    return f->linha.x2;
+}
+
+char *get_y2(void *formato)
+{
+    Forma *f = (Forma *)formato;
+    return f->linha.y2;
+}
+
+char *get_txto(void *formato)
+{
+    Forma *f = (Forma *)formato;
+    return f->texto.txto;
+}
+
+char *get_familia(void *formato)
+{
+    Forma *f = (Forma *)formato;
+    return f->texto.familia;
+}
+
+char *get_tamanho(void *formato)
+{
+    Forma *f = (Forma *)formato;
+    return f->texto.tamanho;
+}
+
+char *get_peso(void *formato)
+{
+    Forma *f = (Forma *)formato;
+    return f->texto.peso;
+}
+
+char *get_ancora(void *formato)
+{
+    Forma *f = (Forma *)formato;
+    return f->texto.ancora;
+}
+
+// Funções de set específicas:
+
+void set_w(void *formato, char w[])
+{
+    Forma *f = (Forma *)formato;
+    strcpy(f->retangulo.w, w);
+}
+
+void set_h(void *formato, char h[])
+{
+    Forma *f = (Forma *)formato;
+    strcpy(f->retangulo.h, h);
+}
+
+void set_r(void *formato, char r[])
+{
+    Forma *f = (Forma *)formato;
+    strcpy(f->circulo.r, r);
+}
+
+void set_x2(void *formato, char x2[])
+{
+    Forma *f = (Forma *)formato;
+    strcpy(f->linha.x2, x2);
+}
+
+void set_y2(void *formato, char y2[])
+{
+    Forma *f = (Forma *)formato;
+    strcpy(f->linha.y2, y2);
+}
+
+void set_txto(void *formato, char txto[])
+{
+    Forma *f = (Forma *)formato;
+    strcpy(f->texto.txto, txto);
+}
+
+void set_familia(void *formato, char familia[])
+{
+    Forma *f = (Forma *)formato;
+    strcpy(f->texto.familia, familia);
+}
+
+void set_tamanho(void *formato, char tamanho[])
+{
+    Forma *f = (Forma *)formato;
+    strcpy(f->texto.tamanho, tamanho);
+}
+
+void set_peso(void *formato, char peso[])
+{
+    Forma *f = (Forma *)formato;
+    strcpy(f->texto.peso, peso);
+}
+
+void set_ancora(void *formato, char ancora[])
+{
+    Forma *f = (Forma *)formato;
+    strcpy(f->texto.ancora, ancora);
+}
 
 
-//rotacionar e etc tbm
+// faz sentido eu precisar receber o tipo antes
 
-//ideia: 
+// rotacionar e etc tbm
+
+// ideia:
